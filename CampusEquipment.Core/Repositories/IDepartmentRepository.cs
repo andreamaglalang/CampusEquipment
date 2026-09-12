dcccc2c8efe823025c3f36ab151c16a3cf4f84cb
andreamaglalang <maglalangpandrea@gmail.com>
2026-09-12 14:51:16 +0800

    Add repository interfaces for Equipment and Department

---
+++ CampusEquipment.Core/Repositories/IDepartmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using CampusEquipment.Core.Entities;
+
+namespace CampusEquipment.Core.Repositories;
+
+public interface IDepartmentRepository
+{
+    Task<IEnumerable<Department>> GetAllAsync();
+    Task<Department?> GetByIdAsync(int id);
+    Task<bool> ExistsAsync(int id);
+}
---
+++ CampusEquipment.Core/Repositories/IEquipmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using CampusEquipment.Core.Entities;
+
+namespace CampusEquipment.Core.Repositories;
+
+public interface IEquipmentRepository
+{
+    Task<IEnumerable<Equipment>> GetAllAsync();
+    Task<Equipment?> GetByIdAsync(int id);
+    Task<Equipment?> GetByAssetCodeAsync(string assetCode);
+    Task<bool> AssetCodeExistsAsync(string assetCode, int? excludeId = null);
+    Task<Equipment> AddAsync(Equipment equipment);
+    Task UpdateAsync(Equipment equipment);
+    Task DeleteAsync(int id);
+}
