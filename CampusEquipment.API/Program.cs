98a0691bfc06ba4762e7f5f6abf3ec40255c5e40
andreamaglalang <maglalangpandrea@gmail.com>
2026-09-12 15:30:15 +0800

    Wire up dependency injection and move connection string to config

--- CampusEquipment.API/Program.cs
+++ CampusEquipment.API/Program.cs
@@ -1,17 +1,30 @@
+using CampusEquipment.Core.Repositories;
+using CampusEquipment.Core.Services;
+using CampusEquipment.Infrastructure.Data;
+using CampusEquipment.Infrastructure.Repositories;
+using CampusEquipment.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IEquipmentRepository, EquipmentRepository>();
+builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+builder.Services.AddScoped<IEquipmentService, EquipmentService>();
+builder.Services.AddScoped<IDepartmentService, DepartmentService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
